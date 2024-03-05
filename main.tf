b053543dd3ae3017a314124c02be3e22e9fb053c
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-03-05 13:34:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ module "aws_cognito_user_pool" {
   user_pool_name = var.user_pool_name
   tags = var.tags
 }
+
+resource "aws_cognito_user_pool_client" "video2mp3_client" {
+  name = "video2mp3-client"
+
+  user_pool_id = module.aws_cognito_user_pool.id
+
+  generate_secret     = true
+  explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
+}
