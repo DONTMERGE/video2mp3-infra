daf4e517a424d0cf3c2e1a3d26cfa6fcca2cb62b
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-04-15 10:44:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ resource "aws_cognito_user_pool_client" "video2mp3_client" {
   generate_secret     = true
   explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
 }
+
+module "lambda_example_code-signing" {
+  source  = "terraform-aws-modules/lambda/aws//examples/code-signing"
+  version = "7.2.6"
+}
