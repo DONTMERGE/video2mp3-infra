6e96ecfef2996f6bf4d2e51c02cf1e67e0cdf24e
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-04-16 17:13:44 -0400

    Add Signer and code s3 bucket

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "aws_cognito_user_pool" {
-  source = "lgallard/cognito-user-pool/aws"
+  source         = "lgallard/cognito-user-pool/aws"
   user_pool_name = var.user_pool_name
-  tags = var.tags
+  tags           = var.tags
 }
 
 resource "aws_cognito_user_pool_client" "video2mp3_client" {
@@ -13,7 +13,38 @@ resource "aws_cognito_user_pool_client" "video2mp3_client" {
   explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
 }
 
-module "lambda_example_code-signing" {
-  source  = "terraform-aws-modules/lambda/aws//examples/code-signing"
-  version = "7.2.6"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket_prefix = "video2mp3_infra-"
+  force_destroy = true
+
+  # S3 bucket-level Public Access Block configuration
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_signer_signing_profile" "video2mp3" {
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  # invalid value for name (must be alphanumeric with max length of 64 characters)
+  name = "video2mp3_infra"
+
+  signature_validity_period {
+    value = 3
+    type  = "MONTHS"
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "user_pool_creation_date" {
   value       = module.aws_cognito_user_pool.creation_date
   description = "Date the Cognito User Pool was created."
 }
+
+output "aws_signer_profile_name" {
+  value       = aws_signer_signing_profile.video2mp3.name
+  description = "Name of the AWS Signer Profile."
+}
+
+output "code_s3_bucket" {
+  value       = module.s3_bucket.s3_bucket_arn
+  description = "Name of the AWS S3 Bucket."
+}
