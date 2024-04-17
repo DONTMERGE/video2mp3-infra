6bdd51ed7ba8f89f1c13f23480402deaad67f28d
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-04-16 20:29:32 -0400

    Remove code commit and create an artifact folder for unsigned code

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,5 +1,5 @@
 data "archive_file" "lambda_get_api" {
   type        = "zip"
   source_file = "${path.module}/code/get_api.js"
-  output_path = "${path.module}/artifacts/get_api.zip"
+  output_path = "${path.module}/code/get_api.zip"
 }
