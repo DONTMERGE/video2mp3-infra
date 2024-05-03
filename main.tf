36c01e270731502f41b53ab83b1e1ca17a87fb9b
srinivaspradhan64 <srinivaspradhan@gmail.com>
2024-05-02 22:20:56 -0400

    Comment out Cognito

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+#module "aws_cognito_user_pool" {
+#  source         = "lgallard/cognito-user-pool/aws"
+#  user_pool_name = var.user_pool_name
+#  tags           = var.tags
+#}
+#
+#resource "aws_cognito_user_pool_client" "video2mp3_client" {
+#  name = "video2mp3-client"
+#
+#  user_pool_id = module.aws_cognito_user_pool.id
+#
+#  generate_secret     = true
+#  explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
+#}
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
-output "user_pool_arn" {
-  value       = module.aws_cognito_user_pool.arn
-  description = "ARN of the Cognito User Pool created."
-}
-
-output "user_pool_name" {
-  value       = module.aws_cognito_user_pool.name
-  description = "Name of the Cognito User Pool created."
-}
-
-output "user_pool_creation_date" {
-  value       = module.aws_cognito_user_pool.creation_date
-  description = "Date the Cognito User Pool was created."
-}
+#output "user_pool_arn" {
+#  value       = module.aws_cognito_user_pool.arn
+#  description = "ARN of the Cognito User Pool created."
+#}
+#
+#output "user_pool_name" {
+#  value       = module.aws_cognito_user_pool.name
+#  description = "Name of the Cognito User Pool created."
+#}
+#
+#output "user_pool_creation_date" {
+#  value       = module.aws_cognito_user_pool.creation_date
+#  description = "Date the Cognito User Pool was created."
+#}
 
 output "aws_signer_profile_name" {
   value       = aws_signer_signing_profile.video2mp3.name
